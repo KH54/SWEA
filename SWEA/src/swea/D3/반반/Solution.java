816282c7d5b0e01a41d2ccb83d6fd8d5cd2b68bc
khPark <pkh543@naver.com>
2022-04-28 16:03:27 +0900

    22/04/28

--- SWEA/src/swea/D3/반반/Solution.java
+++ SWEA/src/swea/D3/반반/Solution.java
@@ -1,10 +1,36 @@
 package swea.D3.반반;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class Solution {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(String[] args) throws NumberFormatException, IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        String str;
+
+        for (int t = 1; t <= T; t++) {
+            str = br.readLine();
+
+            for (int i = 0; i < str.length(); i++) {
+                char temp = str.charAt(i);
+
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + 1);
+                } else {
+                    map.put(temp, 1);
+                }
+            }
+            
+        }
     }
 
 }
