f73222569a33077e6786cb438dbe64eb25fc0000
khPark <pkh543@naver.com>
2022-05-15 23:27:21 +0900

    22/05/15

--- SWEA/src/swea/D3/구독자전쟁/Solution.java
+++ SWEA/src/swea/D3/구독자전쟁/Solution.java
@@ -11,36 +11,33 @@ public class Solution {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 1; t <= T; t++) {
 
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
             int N = Integer.parseInt(st.nextToken());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            int min = 0;
-            int max = 0;
-
-            // A가 3명 B가 5명
-            if (A > B) {
-                min = A - B;
-                max = B;
-            } else if (A < B) {
-                min = B - A;
-                max = A;
-            } else {
-                min = A;
-                max = A;
+            if (B > A) {
+                int temp = B;
+                B = A;
+                A = temp;
             }
+
+            int max = B;
+            int min = 0;
+
+            if (A + B >= N) {
+                max = A + B > N / 2 ? B : A;
+                min = A == B && B == N ? A : A + B > N ? A + B - N : 0;
+            }
+
             sb.append("#").append(t).append(" ").append(max).append(" ").append(min).append("\n");
         }
-        
         System.out.println(sb);
     }
-
 }
