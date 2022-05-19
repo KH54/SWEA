2766e67b90ae78f415f80069b141d4869941fe29
khPark <pkh543@naver.com>
2022-05-19 18:26:52 +0900

    22/05/19

--- SWEA/src/swea/D3/미니멀리즘시계/Solution.java
+++ SWEA/src/swea/D3/미니멀리즘시계/Solution.java
@@ -1,9 +1,8 @@
-package swea.D3.과자분배;
+package swea.D3.미니멀리즘시계;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution {
 
@@ -15,19 +14,15 @@ public class Solution {
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            int radi = Integer.parseInt(br.readLine()) * 2;
 
-            int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-
-            int res = 0;
-            if (n % k != 0) {
-                res = 1;
-            }
-
-            sb.append("#").append(t).append(" ").append(res).append("\n");
+            int h = radi / 60;
+            int m = radi - h * 60;
+            
+            sb.append("#").append(t).append(" ").append(h == 12 ? 0 : h).append(" ").append(m).append("\n");
         }
-        sb.setLength(sb.length() - 1);
+        sb.setLength(sb.length()-1);
         System.out.println(sb);
     }
+
 }
