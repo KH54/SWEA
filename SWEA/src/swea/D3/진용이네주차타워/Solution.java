bf445784f5c801a497c123d289d6a45155d21b26
khPark <pkh543@naver.com>
2022-05-31 16:36:47 +0900

    2022/05/31

---
+++ SWEA/src/swea/D3/진용이네주차타워/Solution.java
@@ -0,0 +1,52 @@
+package swea.D3.진용이네주차타워;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int m = Integer.parseInt(br.readLine());
+
+            int[][] Ri = new int[n][2];
+            int[] Wi = new int[m];
+
+            for (int r = 0; r < n; r++) {
+                Ri[r][0] = Integer.parseInt(br.readLine());
+            }
+
+            for (int w = 0; w < m; w++) {
+                Wi[w] = Integer.parseInt(br.readLine());
+            }
+
+            Queue<Integer> queue = new LinkedList<>();
+
+            int count = 0;
+            int num = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < m * 2; i++) {
+                if (num > 0) {
+                    for (int r = 0; r < n; r++) {
+                        if (Ri[r][0] == 0) {
+                            Ri[r][0] = num;
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
